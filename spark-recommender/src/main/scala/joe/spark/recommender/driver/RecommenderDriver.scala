11e60d8808a7df3ec1e033619e257da6bc8470d6
Joe Sicree <jsicree@gmail.com>
2016-10-18 22:11:26 -0400

    Created skeleton code

--- spark-recommender/src/main/scala/joe/spark/recommender/driver/RecommenderDriver.scala
+++ spark-recommender/src/main/scala/joe/spark/recommender/driver/RecommenderDriver.scala
@@ -1,5 +1,36 @@
 package joe.spark.recommender.driver
 
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.SparkSession
+import joe.spark.recommender._
+
+/**
+ * 
+ * spark-submit --class joe.spark.recommender.driver.RecommenderDriver <path-to-jar>\spark-recommender-0.0.1-SNAPSHOT.jar
+ * 
+ */
+
 object RecommenderDriver {
-  
+
+  def main(args: Array[String]) {
+    println("Starting Recommender")
+    println("Using conf file: " + RECOMMENDER_CONF_FILE)
+    val conf = ConfigFactory.load(RECOMMENDER_CONF_FILE)
+
+    val appName = conf.getString("recommender.appName")
+    val sparkMaster = conf.getString("recommender.sparkMaster")
+    val sparkWarehouseDir = conf.getString("recommender.sparkWarehouseDir")
+    val sparkLocalDir = conf.getString("recommender.sparkLocalDir")
+//    val inputFileName = conf.getString("stockdriver.input.fileName")
+
+    val sparkSession = SparkSession.builder.
+      master(sparkMaster).
+      appName(appName).
+      config("spark.sql.warehouse.dir", sparkWarehouseDir).
+      getOrCreate()
+      
+    sparkSession.stop()
+    
+  }
+
 }
--- spark-recommender/src/main/scala/joe/spark/recommender/package.scala
+++ spark-recommender/src/main/scala/joe/spark/recommender/package.scala
@@ -1,5 +1,7 @@
 package joe.spark
 
 package object recommender {
+
+  final val RECOMMENDER_CONF_FILE = "recommender.conf"
   
 }
