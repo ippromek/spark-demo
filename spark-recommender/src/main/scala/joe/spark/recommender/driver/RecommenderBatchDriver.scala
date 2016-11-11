fd212bfb8eb4317219070481eb1ae2d9ed091f12
Joe Sicree <jsicree@gmail.com>
2016-11-10 22:24:33 -0500

    Added batch specific conf and driver class

--- spark-recommender/src/main/scala/joe/spark/recommender/driver/RecommenderBatchDriver.scala
+++ spark-recommender/src/main/scala/joe/spark/recommender/driver/RecommenderBatchDriver.scala
@@ -11,31 +11,28 @@ import joe.spark.recommender.processor.RecommendationEngine
 
 /**
  *
- * spark-submit --master local[*] --class joe.spark.recommender.driver.RecommenderDriver --conf spark.sql.warehouse.dir=<path-to-warehouse-dir> <path-to-jar>\spark-recommender-0.0.1-SNAPSHOT.jar <userId>
+ * spark-submit --master local[*] --class joe.spark.recommender.driver.RecommenderBatchDriver --conf spark.sql.warehouse.dir=<path-to-warehouse-dir> <path-to-jar>\spark-recommender-0.0.1-SNAPSHOT.jar
  *
  */
 
-object RecommenderDriver {
+object RecommenderBatchDriver {
 
-  @transient lazy val log = org.apache.log4j.LogManager.getLogger(RecommenderDriver.getClass.getName)
+  @transient lazy val log = org.apache.log4j.LogManager.getLogger(RecommenderBatchDriver.getClass.getName)
 
   def main(args: Array[String]) {
-    log.info("Starting Recommender")
-    log.info("Using conf file: " + RECOMMENDER_CONF_FILE)
-    val conf = ConfigFactory.load(RECOMMENDER_CONF_FILE)
+    log.info("Starting RecommenderBatchDriver")
+    log.info("Using conf file: " + RECOMMENDER_BATCH_CONF_FILE)
+    val conf = ConfigFactory.load(RECOMMENDER_BATCH_CONF_FILE)
 
-    val appName = conf.getString("recommender.appName")
-    val numberOfRecommendations = conf.getInt("recommender.numRecommendations")
-    val userId = args(0).toLong
-    
-    log.info("userId: " + userId)
+    val appName = conf.getString("recommender.batch.appName")
+    val numberOfRecommendations = conf.getInt("recommender.batch.numRecommendations")    
+
     log.info("numberOfRecommendations: " + numberOfRecommendations)
     
     // Create SparkSession
     val sparkSession = SparkSession.builder.
       appName(appName).
       getOrCreate()
-
 
     if (log.isInfoEnabled()) {
       log.info("SparkSession Conf:")
@@ -57,20 +54,20 @@ object RecommenderDriver {
     // 1000246
     // 1000177 (small)
     // 1000142 (small)
-
-    val listeningHistoryRdd = RecommendationEngine.getListeningHistory(userId, userArtistRdd, artistRdd)
+    
+//    val listeningHistoryRdd = RecommendationEngine.getListeningHistory(userId, userArtistRdd, artistRdd)
 
     log.info("========================================")
     log.info("Listening History:")
-    listeningHistoryRdd.collect().foreach(x => log.info(x.name + " (" + x.artistId + ")"))
+//    listeningHistoryRdd.collect().foreach(x => log.info(x.name + " (" + x.artistId + ")"))
 
     val model = RecommendationEngine.createModel(userArtistRdd, sparkSession, artistAliasBc)
 
-    val recommendationsRdd = RecommendationEngine.getRecommendations(userId, numberOfRecommendations, model, userArtistRdd, artistRdd)
+//    val recommendationsRdd = RecommendationEngine.getRecommendations(userId, numberOfRecommendations, model, userArtistRdd, artistRdd)
 
     log.info("========================================")
     log.info("Recommendations:")
-    recommendationsRdd.collect().foreach(x => log.info(x.name + " (" + x.artistId + ")"))
+//    recommendationsRdd.collect().foreach(x => log.info(x.name + " (" + x.artistId + ")"))
 
     log.info("========================================")
 
--- spark-recommender/src/main/scala/joe/spark/recommender/package.scala
+++ spark-recommender/src/main/scala/joe/spark/recommender/package.scala
@@ -3,6 +3,7 @@ package joe.spark
 package object recommender {
 
   final val RECOMMENDER_CONF_FILE = "recommender.conf"
+  final val RECOMMENDER_BATCH_CONF_FILE = "recommender_batch.conf"
   
   final case class UserArtistRecord(userId: Long, 
                       artistId: Long, 
